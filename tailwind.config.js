813d8c5e1b1e83c156d6c7bfb987afbbcbbefaf6
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-01-14 17:43:54 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      
+    },
+    boxShadow: {
+      '3xl': '0px 4px 10px rgba(0, 0, 0, 0.12);',
+      'drop': '0px 4px 6px rgba(0, 0, 0, 0.12);',
+    },
+    gridTemplateRows: {
+      'foot' : 'repeat(4, minmax(0, auto));',
+    },
+  },
+  plugins: [],
+}
